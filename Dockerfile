c95b26c859a789388b5c034f50983f3d8de3aaeb
Rhys Adams <rhys@therhys.co.uk>
2023-11-22 11:52:57 +0000

    Auto scss compiling not working.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM docker.io/tekki/mojolicious
 
-RUN cpan -T Text::Markdown
+RUN cpan -T Text::Markdown Mojolicious::Plugin::AssetPack Mojolicious::Plugin::AssetPack::Pipe::Sass
+
+# RUN apt-get update && apt-get install -y ruby-sass 
+RUN curl -L --output dartsass.tar.gz https://github.com/sass/dart-sass/releases/download/1.69.5/dart-sass-1.69.5-linux-x64.tar.gz \
+    && tar -C /opt -xzf dartsass.tar.gz
+
+ENV PATH="${PATH}:/opt/dart-sass"
 
 ENTRYPOINT ["script/site", "prefork", "-m", "production"]
--- lib/TheRhys.pm
+++ lib/TheRhys.pm
@@ -1,5 +1,8 @@
 package TheRhys;
 
+use File::Basename;
+use Data::Dumper;
+
 use Mojo::Base 'Mojolicious', -signatures;
 
 use TheRhys::Model::Blog;
@@ -11,6 +14,22 @@ sub startup {
     my ($self) = @_;
 
     my $config = $self->plugin('NotYAMLConfig');
+    my $assets = $self->plugin('AssetPack', {pipes => [qw(Sass Css Combine)]});
+
+    for(glob("@{[$self->home]}/assets/*")) {
+        my $src = basename($_);
+        $self->asset->store->extra($src => "assets/$src");
+    }
+
+    for(glob("@{[$self->home]}/assets/*.scss")) {
+        next if (/_.*$/);
+        my $src = basename($_);
+        (my $product = $src) =~ s/\.scss$/.css/;
+        # say "$src => $product"; 
+        # $assets{$src} = $_; 
+        say "Register $product";
+        $self->asset->process($product => $src);
+    }
 
     $self->log->level('error');
 
